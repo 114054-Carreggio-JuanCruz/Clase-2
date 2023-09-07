84369d744d2fc55977f86f1d27a42fec264deb82
114054-Carreggio-JuanCruz <129430523+114054-Carreggio-JuanCruz@users.noreply.github.com>
2023-09-07 12:10:48 -0300

    Resolucion de ambos ejercicios

--- EstructurasCondicionales/Ejercicios-Tarea/FizzBuzz.cs
+++ EstructurasCondicionales/Ejercicios-Tarea/FizzBuzz.cs
@@ -5,13 +5,22 @@ public class FizzBuzz{
     
     public void EjecutarFizzBuzz(){
         
+        string mostrar="";
         for (int i = 1; i < 101; i++)
         {
-           int resto3 ;
-           Math.DivRem(i, 3, out resto3);
-           int resto5;
-           Math.DivRem(i, 3, out resto5);
-           if(Math.)
+           if(i%3==0 && i%5==0){
+            mostrar = "Fizzbuzz";
+           }
+           else if(i%3==0){
+            mostrar = "Fizz";
+           }
+           else if(i%5==0){
+            mostrar = "Buzz";
+           }
+           else{
+            mostrar= i.ToString();
+           }
+           Console.Write($"{mostrar} ");
         }
     }
 }
